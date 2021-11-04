71195e93337cb8f6082e8441a239af6e1d008494
dalin.hoem <dalin.hoem@mango-byte.com>
2021-11-04 15:23:34 +0700

    table relationship

--- mobile_final.sql
+++ mobile_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2021 at 03:25 AM
+-- Generation Time: Nov 04, 2021 at 09:21 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -24,48 +24,178 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_users`
+-- Table structure for table `tbl_category`
 --
 
-CREATE TABLE `tbl_users` (
+CREATE TABLE `tbl_category` (
   `id` int(11) NOT NULL,
-  `fullname` text NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` text NOT NULL,
-  `email` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL,
+  `des` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `tbl_users`
+-- Table structure for table `tbl_customers`
 --
 
-INSERT INTO `tbl_users` (`id`, `fullname`, `username`, `password`, `email`) VALUES
-(1, 'female', 'dalin', '202cb962ac59075b964b07152d234b70', 'dalin@gmail.com'),
-(2, 'raiyasous', 'raiya', '202cb962ac59075b964b07152d234b70', 'raiya@gmail.com'),
-(3, 'fullname', 'password', '$2y$10$mmQF4dn9yWQR.tkhxzWcVuf31ftSZ5Bo2wpeAOGn9pRKboFWYKFo6', 'username'),
-(7, 'Jennie Kim', 'jennie', '$2y$10$zyj8k4a4RLL7J4ZKJ5koW.Lx5KR6wGmZNEGLVedj1epK2cotr3WrG', 'jennie@gmail.com');
+CREATE TABLE `tbl_customers` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_orders`
+--
+
+CREATE TABLE `tbl_orders` (
+  `id` int(11) NOT NULL,
+  `cus_id` int(11) DEFAULT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `shiping_address` varchar(255) NOT NULL,
+  `order_address` varchar(255) NOT NULL,
+  `order_date` date NOT NULL,
+  `order_status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order_details`
+--
+
+CREATE TABLE `tbl_order_details` (
+  `id` int(11) NOT NULL,
+  `pro_id` int(11) DEFAULT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_products`
+--
+
+CREATE TABLE `tbl_products` (
+  `id` int(11) NOT NULL,
+  `cat_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `des` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `weight` decimal(10,2) NOT NULL,
+  `height` decimal(10,2) NOT NULL,
+  `stock` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tbl_users`
+-- Indexes for table `tbl_category`
 --
-ALTER TABLE `tbl_users`
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_customers`
+--
+ALTER TABLE `tbl_customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_orders`
+--
+ALTER TABLE `tbl_orders`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD KEY `cus_id` (`cus_id`);
+
+--
+-- Indexes for table `tbl_order_details`
+--
+ALTER TABLE `tbl_order_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `pro_id` (`pro_id`);
+
+--
+-- Indexes for table `tbl_products`
+--
+ALTER TABLE `tbl_products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cat_id` (`cat_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tbl_users`
+-- AUTO_INCREMENT for table `tbl_category`
 --
-ALTER TABLE `tbl_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `tbl_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_customers`
+--
+ALTER TABLE `tbl_customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_orders`
+--
+ALTER TABLE `tbl_orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_order_details`
+--
+ALTER TABLE `tbl_order_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_products`
+--
+ALTER TABLE `tbl_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_orders`
+--
+ALTER TABLE `tbl_orders`
+  ADD CONSTRAINT `tbl_orders_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `tbl_customers` (`id`);
+
+--
+-- Constraints for table `tbl_order_details`
+--
+ALTER TABLE `tbl_order_details`
+  ADD CONSTRAINT `tbl_order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `tbl_orders` (`id`),
+  ADD CONSTRAINT `tbl_order_details_ibfk_2` FOREIGN KEY (`pro_id`) REFERENCES `tbl_products` (`id`);
+
+--
+-- Constraints for table `tbl_products`
+--
+ALTER TABLE `tbl_products`
+  ADD CONSTRAINT `tbl_products_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `tbl_category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
